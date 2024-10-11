609f287ca92784c9737218c88f59b55998b0ff1b
Saskia Hack <saskia.hack@protonmail.com>
2024-10-11 17:01:40 +0200

    -added structex encoding for bitfield -revoked nested struct into one, because structex does everything for me

--- main.go
+++ main.go
@@ -3,19 +3,9 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 
 	"github.com/HewlettPackard/structex"
 )
-
-type bitfield struct {
-	sMSrequested                     uint8 `bitfield:"1,"`
-	nG_RAN_RCU                       uint8 `bitfield:"1,"`
-	preferred_CIoT_Network_Behaviour uint8 `bitfield:"2,"`
-	ePS_PNB_CIoT                     uint8 `bitfield:"2,"`
-	spare1                           uint8 `bitfield:"1,reserved"`
-	spare2                           uint8 `bitfield:"1,reserved"`
-}
 
 type nas5GSUpdateType struct {
 
@@ -26,7 +16,12 @@ type nas5GSUpdateType struct {
 	updateTypeLength byte
 
 	//third octet
-	bitfield
+	sMSrequested                     uint8 `bitfield:"1,"`
+	nG_RAN_RCU                       uint8 `bitfield:"1,"`
+	preferred_CIoT_Network_Behaviour uint8 `bitfield:"2,"`
+	ePS_PNB_CIoT                     uint8 `bitfield:"2,"`
+	spare1                           uint8 `bitfield:"1,reserved"`
+	spare2                           uint8 `bitfield:"1,reserved"`
 }
 
 func main() {
@@ -34,32 +29,29 @@ func main() {
 	var test nas5GSUpdateType
 	test.updateTypeIEI = 1
 	test.updateTypeLength = 2
-	test.bitfield.ePS_PNB_CIoT = 0
-	test.bitfield.preferred_CIoT_Network_Behaviour = 0
-	test.bitfield.ePS_PNB_CIoT = 1
-	test.bitfield.sMSrequested = 1
+	test.ePS_PNB_CIoT = 0
+	test.preferred_CIoT_Network_Behaviour = 0
+	test.nG_RAN_RCU = 1
+	test.sMSrequested = 1
 
 	var buffer bytes.Buffer
 	bufferPtr := &buffer
 
-	Encode(test, bufferPtr)
-
+	test.Encode(bufferPtr)
 }
 
-func Encode(ie nas5GSUpdateType, buffer *bytes.Buffer) {
+func (ie nas5GSUpdateType) Encode(buffer *bytes.Buffer) {
 
-	//write first octet in buffer
-	fmt.Println("Update Type: ", ie.updateTypeIEI)
-	buffer.WriteByte(ie.updateTypeIEI)
+	byteArray, err := structex.EncodeByteBuffer(ie)
 
-	//write second octet in buffer
-	fmt.Println("Update Type length: ", ie.updateTypeLength)
-	buffer.WriteByte(ie.updateTypeLength)
+	if err != nil {
+		panic("Encoding not possible.")
+	}
 
-	fmt.Println("Buffered Stuff ", buffer.String())
+	for index, element := range byteArray {
+		buffer.WriteByte(element)
+		fmt.Println("Byte index ", index, " Element: ", element)
+	}
 
-	//convert bitfield and add to buffer
-	var writer io.ByteWriter
-	structex.Encode(writer, ie.bitfield)
-	fmt.Println(writer)
+	fmt.Println(buffer)
 }
